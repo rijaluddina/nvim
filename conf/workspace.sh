7f26678d2144c0ff0fec9014fba01460cc14e2de
rijaluddina <rijaluddin.alazmi@outlook.com>
2024-10-21 01:41:39 +0700

    move to dotfile

--- conf/workspace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Open Visual Studio Code
-code &
-
-# Open browser ( Firefox / Google Chrome)
-google-chrome-stable &
-
-# Give execute permission to the script
-# chmod +x /home/rijaluddina/workspace.sh
